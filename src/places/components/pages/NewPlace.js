62f3afc7e3c9461e2a0356dfdc502057e44db218
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-09-21 16:21:56 -0300

    useCallback and useEffect in input and newplaces

--- src/places/components/pages/NewPlace.js
+++ src/places/components/pages/NewPlace.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { VALIDATOR_MINLENGTH } from '../../../shared/util/validators';
 
 import Input from '../../shared/components/FormElements/Input';
 import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
@@ -13,6 +18,15 @@ const NewPlace = () => {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+       <Input
+        element="textarea"
+        id="description"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 caracteres)."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
 import './Input.css';
@@ -62,6 +62,13 @@ const Input = props => {
         value={inputState.value}
       />
     );
+
+      const {id, onInput} = props;
+      const {value, isValid} = inputState;
+
+      useEffect(() => {
+          onInput(id, value, isValid)
+      }, [id, value, isValid, onInput])
 
   return (
     <div
