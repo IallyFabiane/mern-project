cdc624a0dd31f68078af52750e0ca9d86affb870
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-09-21 15:58:17 -0300

    validators

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import NewPlace from './places/components/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+
 import Users from './user/pages/Users';
-import UserPlaces from './places/components/pages/UserPlaces';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
@@ -25,6 +31,6 @@ const App = () => {
       </main>
     </Router>
   );
-}
+};
 
 export default App;
