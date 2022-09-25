c6be6979af97ae071b50854d1b7356d95eb25e1d
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-09-24 21:32:00 -0300

    sigup and login

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,40 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import './Auth.css';
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
-import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from "../../shared/util/validators";
+import { VALIDATOR_EMAIL,  VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 
 const Auth = () => {
-
-   const [formState, inputHandler] = useForm({
+    const [isLoginMode, setIsLoginMode] = useState(true);
+  
+    const [formState, inputHandler, setFormData] = useForm(
+      {
         email: {
-            value:'',
-            isValid:false
+          value: '',
+          isValid: false
         },
         password: {
-            value:'',
-            isValid:false
+          value: '',
+          isValid: false
         }
-    }, false);
-
-    const authSubmitHandler = (event) => {
-        event.preventDefault();
-        console.log(formState.inputs);
-    }
-
+      },
+      false
+    );
+  
+    const switchModeHandler = () => {
+      if (!isLoginMode) {
+        setFormData(
+          {
+            ...formState.inputs,
+            name: undefined
+          },
+          formState.inputs.email.isValid && formState.inputs.password.isValid
+        );
+      } else {
+        setFormData(
+          {
+            ...formState.inputs,
+            name: {
+              value: '',
+              isValid: false
+            }
+          },
+          false
+        );
+      }
+      setIsLoginMode(prevMode => !prevMode);
+    };
+  
+    const authSubmitHandler = event => {
+      event.preventDefault();
+      console.log(formState.inputs);
+    };
+  
     return (
-        <Card>
-            <h2>Login Required</h2>
-            <hr />
-           <form onSubmit={authSubmitHandler}>
-                <Input onInput={inputHandler} id="email" element="input" type="email" label="E-mail" validators={[VALIDATOR_EMAIL()]} errorText="PLease enter a valid e-mail address."/>
-                <Input onInput={inputHandler} id="password" element="input" type="password" label="Password" validators={[VALIDATOR_MINLENGTH(5)]} errorText="PLease enter a valid password, at least 5 characters."/>
-                <Button type="submit" disabled={!formState.isValid} >LOGIN</Button>
-           </form>
-        </Card>
-    )
-}
-
-export default Auth;
+      <Card className="authentication">
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter a name."
+              onInput={inputHandler}
+            />
+          )}
+          <Input
+            element="input"
+            id="email"
+            type="email"
+            label="E-Mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email address."
+            onInput={inputHandler}
+          />
+          <Input
+            element="input"
+            id="password"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password, at least 5 characters."
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+        </Button>
+      </Card>
+    );
+  };
+  
+  export default Auth;
+  
